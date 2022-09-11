e4bee92b349252fc08198ad0f78e0d3c54e63fca
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2022-09-11 18:39:36 +0300

    adaptiv added

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,10 @@
+export class BurgerMenu {
+  constructor(navBlock, burerButton, closeButton, logo) {
+    this.navBlock = document.querySelector('nav')
+    this.burerButton = burerButton;
+    this.closeButton = closeButton;
+    this.logo = logo;
+    this.logoVisibleClass = logoVisibleClass
+  }
+_
+}
